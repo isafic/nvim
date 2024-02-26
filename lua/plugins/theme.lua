15ad0a9446ff8cb726605819767d63e4acd683bd
Isa Fichtinger <ficcdaf@gmail.com>
2024-02-25 22:24:16 -0500

    Change to kanagawa theme, move theme config to own file

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = false,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "rebelot/kanagawa.nvim",
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-dragon",
+    },
+  },
+}
