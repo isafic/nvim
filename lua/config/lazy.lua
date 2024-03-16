6c1256db05b6bf3f0f03225ac1c6b9e8a073dbb2
Isa Fichtinger <ficcdaf@gmail.com>
2024-03-16 16:39:29 -0400

    change edgy, fix clang

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -46,3 +46,13 @@ require("lazy").setup({
     },
   },
 })
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+require("lspconfig").clangd.setup({
+  on_attach = on_attach,
+  capabilities = cmp_nvim_lsp.default_capabilities(),
+  cmd = {
+    "clangd",
+    "--offset-encoding=utf-16",
+  },
+})
